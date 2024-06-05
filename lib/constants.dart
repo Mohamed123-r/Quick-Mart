57f9800b891f9ae85d177c8ec98f0a1c6f54da54
mohamed <mezoattalla123@gmail.com>
2024-06-05 12:31:17 +0300

    Updated the color constants in constants.dart to include a wider range of colors for the Quick Mart app. Added cyan, black, white, red, blue, purple, yellow, orange, merigold, brown, pink, and various shades of grey.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/animation.dart';
 
-const kPrimaryColor = Color(0xff53B175);
-const kGreyColor = Color(0xff7C7C7C);
+const kCyanColor = Color(0xFF21D4B4);
+const kCyan_50Color = Color(0xFFF4FDFA);
+const kBlackColor = Color(0xFF1C1B1B);
+const kWhiteColor = Color(0xFFFFFFFF);
+const kRedColor = Color(0xFFEE4D4D);
+const kBlueColor = Color(0xFF1F88DA);
+const kPurpleColor = Color(0xFF4F1FDA);
+const kYellowColor = Color(0xFFEBEF14);
+const kOrangeColor = Color(0xFFF0821D);
+const kMerigoldColor = Color(0xFFFFCB45);
+const kBrownColor = Color(0xFF5A1A05);
+const kPinkColor = Color(0xFFCE1DEB);
+const kGrey_50Color = Color(0xFFF4F5FD);
+const kGrey_100Color = Color(0xFFC0C0C0);
+const kGrey_150Color = Color(0xFF6F7384);
