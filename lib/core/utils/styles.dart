73102468405e7f1c0d5e069ac360a951c02445b0
mohamed <mezoattalla123@gmail.com>
2024-06-05 15:37:45 +0300

    Updated text styles for headings in styles.dart file. Added new heading styles with different font weights and sizes.

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,9 +2,61 @@ import 'package:flutter/material.dart';
 
 abstract class Styles {
 
-  static const textStyle16 = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.w600,
+  static const heading1Bold = TextStyle(
+    fontSize: 32.0,
+    fontWeight: FontWeight.bold,
+    height: 1.2,
+    letterSpacing: 0,
   );
+  static const heading1SemiBold = TextStyle(
+    fontSize: 32.0,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+    letterSpacing: 0,
+  );
+  static const heading1Regular = TextStyle(
+    fontSize: 32.0,
+    fontWeight: FontWeight.w400,
+    height: 1.2,
+    letterSpacing: 0,
+  );
+
+  static const heading2Bold = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+    height: 1.2,
+    letterSpacing: 0,
+  );
+  static const heading2SemiBold = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+    letterSpacing: 0,
+  );
+  static const heading2Regular = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w400,
+    height: 1.2,
+    letterSpacing: 0,
+  );
+  static const heading3Bold = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+    height: 1.2,
+    letterSpacing: 0.25,
+  );
+  static const heading3SemiBold = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+    letterSpacing: 0.25,
+  );
+  static const heading3Regular = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w400,
+    height: 1.2,
+    letterSpacing: 0.25,
+  );
+
 
 }
