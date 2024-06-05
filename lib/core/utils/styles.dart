114b2275956873992ecb06bdf50f0da0cce20f37
mohamed <mezoattalla123@gmail.com>
2024-06-05 15:43:38 +0300

    Refactor button styles in styles.dart and add button1SemiBold and button2SemiBold text styles.

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-
   static const heading1Bold = TextStyle(
     fontSize: 32.0,
     fontWeight: FontWeight.bold,
     height: 1.2,
     letterSpacing: 0,
   );
+
   static const heading1SemiBold = TextStyle(
     fontSize: 32.0,
     fontWeight: FontWeight.w600,
     height: 1.2,
     letterSpacing: 0,
   );
+
   static const heading1Regular = TextStyle(
     fontSize: 32.0,
     fontWeight: FontWeight.w400,
@@ -27,30 +28,35 @@ abstract class Styles {
     height: 1.2,
     letterSpacing: 0,
   );
+
   static const heading2SemiBold = TextStyle(
     fontSize: 24.0,
     fontWeight: FontWeight.w600,
     height: 1.2,
     letterSpacing: 0,
   );
+
   static const heading2Regular = TextStyle(
     fontSize: 24.0,
     fontWeight: FontWeight.w400,
     height: 1.2,
     letterSpacing: 0,
   );
+
   static const heading3Bold = TextStyle(
     fontSize: 18.0,
     fontWeight: FontWeight.bold,
     height: 1.2,
     letterSpacing: 0.25,
   );
+
   static const heading3SemiBold = TextStyle(
     fontSize: 18.0,
     fontWeight: FontWeight.w600,
     height: 1.2,
     letterSpacing: 0.25,
   );
+
   static const heading3Regular = TextStyle(
     fontSize: 18.0,
     fontWeight: FontWeight.w400,
@@ -58,5 +64,15 @@ abstract class Styles {
     letterSpacing: 0.25,
   );
 
+  static const button1SemiBold = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0,
+  );
 
+  static const button2SemiBold = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0,
+  );
 }
