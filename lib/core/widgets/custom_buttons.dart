b23f010f99015206697f911177da4aad229d6488
mohamed <mezoattalla123@gmail.com>
2024-07-19 21:23:59 +0300

    Add primary and secondary bottom custom buttons

---
+++ lib/core/widgets/custom_buttons.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/utils/styles.dart';
+
+class PrimaryBottom extends StatelessWidget {
+  const PrimaryBottom(
+      {super.key, required this.title, required this.onPressed});
+
+  final Function() onPressed;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: MaterialButton(
+        height: 60,
+        color: kBlackColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          title,
+          style: Styles.button2SemiBold.copyWith(
+            color: kWhiteColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondaryBottom extends StatelessWidget {
+  const SecondaryBottom(
+      {super.key, required this.title, required this.onPressed});
+
+  final Function() onPressed;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: MaterialButton(
+        elevation: 0,
+        height: 60,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+          side: const BorderSide(
+            color: kGrey_50Color,
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(title, style: Styles.button2SemiBold),
+      ),
+    );
+  }
+}
