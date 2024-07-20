3fb6d78bec689d4b9b5845602a31c15fe2e84fe0
mohamed <mezoattalla123@gmail.com>
2024-07-20 19:47:54 +0300

    * Added several SVG icons for use in the application * Updated the `Assets` class to include the new icons * Updated the `Assets` class to include the new icons

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,15 @@
 abstract class Assets {
   static String searchIcon = "assets/images/search-normal.svg";
   static String filterIcon = "assets/images/filter_icon.svg";
+  static String homeIcon = "assets/images/home-icon.svg";
+  static String profileIcon = "assets/images/profile_icon.svg";
+  static String heartIcon = "assets/images/heart-icon.svg";
+  static String categoryIcon = "assets/images/category-icon.svg";
+  static String shoppingCartIcon = "assets/images/shopping-cart-icon.svg";
+  static String home2Icon = "assets/images/home2_icon.svg";
+  static String profile2Icon = "assets/images/profile2-icon.svg";
+  static String heart2Icon = "assets/images/heart2_icon.svg";
+  static String category2Icon = "assets/images/category2_icon.svg";
+  static String shoppingCart2Icon = "assets/images/shopping-cart2_icon.svg";
+
 }
---
+++ lib/core/widgets/custom_bottom_navigation_bar.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/utils/assets.dart';
+import 'package:quick_mart/core/utils/styles.dart';
+import 'package:quick_mart/core/widgets/custom_progress_indicators.dart';
+
+class CustomBottomNavigationBar extends StatefulWidget {
+  const CustomBottomNavigationBar({
+    super.key,
+  });
+
+  @override
+  State<CustomBottomNavigationBar> createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: SizedBox(
+          height: 60,
+          child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.white,
+              selectedLabelStyle: Styles.captionSemiBold,
+              selectedItemColor: kBlackColor,
+              unselectedItemColor: kGrey_150Color,
+              currentIndex: _selectedIndex,
+              onTap: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              items: [
+                BottomNavigationBarItem(
+                  icon: SvgPicture.asset(
+                    _selectedIndex != 0 ? Assets.homeIcon : Assets.home2Icon,
+                  ),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: SvgPicture.asset(
+                    _selectedIndex != 1
+                        ? Assets.categoryIcon
+                        : Assets.category2Icon,
+                  ),
+                  label: 'Categories',
+                ),
+                BottomNavigationBarItem(
+                  icon: SvgPicture.asset(
+                    _selectedIndex != 2
+                        ? Assets.shoppingCartIcon
+                        : Assets.shoppingCart2Icon,
+                  ),
+                  label: 'My Cart',
+                ),
+                BottomNavigationBarItem(
+                  icon: SvgPicture.asset(
+                    _selectedIndex != 3 ? Assets.heartIcon : Assets.heart2Icon,
+                  ),
+                  label: 'Wishlist',
+                ),
+                BottomNavigationBarItem(
+                  icon: SvgPicture.asset(
+                    _selectedIndex != 4
+                        ? Assets.profileIcon
+                        : Assets.profile2Icon,
+                  ),
+                  label: 'Profile',
+                ),
+              ]),
+        ),
+        body: <Widget>[
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.blueAccent,
+          ),
+          Container(
+            color: Colors.green,
+          ),
+          Container(
+            color: Colors.purple,
+          )
+        ][_selectedIndex]);
+  }
+}
