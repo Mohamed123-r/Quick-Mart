d213a3c33cba11da6e0bd3714644d23ec90022e4
mohamed <mezoattalla123@gmail.com>
2024-07-20 21:10:43 +0300

    "Update CustomInputField widget to make it stateless and add title property"

--- lib/core/widgets/custom_input_field.dart
+++ lib/core/widgets/custom_input_field.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quick_mart/constants.dart';
 import 'package:quick_mart/core/utils/styles.dart';
 
-class CustomInputField extends StatefulWidget {
+class CustomInputField extends StatelessWidget {
   const CustomInputField(
       {super.key,
       required this.hintText,
@@ -10,8 +10,10 @@ class CustomInputField extends StatefulWidget {
       this.obscureText = false,
       this.validator,
       this.controller,
-      this.keyboardType});
+      this.keyboardType,
+      required this.title});
 
+  final String title;
   final String hintText;
   final IconData? suffixIcon;
   final bool obscureText;
@@ -20,55 +22,72 @@ class CustomInputField extends StatefulWidget {
   final TextInputType? keyboardType;
 
   @override
-  State<CustomInputField> createState() => _CustomInputFieldState();
-}
-
-class _CustomInputFieldState extends State<CustomInputField> {
-  @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      obscureText: widget.obscureText,
-      validator: widget.validator,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      controller: widget.controller,
-      keyboardType: widget.keyboardType,
-      cursorColor: kCyanColor,
-      decoration: InputDecoration(
-        contentPadding: const EdgeInsets.symmetric(
-          vertical: 18.0,
-          horizontal: 16.0,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Text(
+              title,
+              style: Styles.body2Medium,
+            ),
+            Text(
+              ' *',
+              style: Styles.body2Medium.copyWith(
+                color: kRedColor,
+              ),
+            ),
+          ],
         ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-          borderSide: const BorderSide(
-            color: kGrey_50Color,
+        const SizedBox(
+          height: 8.0,
+        ),
+        TextFormField(
+          obscureText: obscureText,
+          validator: validator,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          controller: controller,
+          keyboardType: keyboardType,
+          cursorColor: kCyanColor,
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(
+              vertical: 18.0,
+              horizontal: 16.0,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              borderSide: const BorderSide(
+                color: kGrey_50Color,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              borderSide: const BorderSide(
+                color: kGrey_50Color,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              borderSide: const BorderSide(
+                color: kCyanColor,
+              ),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              borderSide: const BorderSide(
+                color: kRedColor,
+              ),
+            ),
+            hintText: hintText,
+            hintStyle: Styles.captionRegular.copyWith(
+              color: kGrey_100Color,
+            ),
+            filled: true,
+            fillColor: kWhiteColor,
           ),
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-          borderSide: const BorderSide(
-            color: kGrey_50Color,
-          ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-          borderSide: const BorderSide(
-            color: kCyanColor,
-          ),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.0),
-          borderSide: const BorderSide(
-            color: kRedColor,
-          ),
-        ),
-        hintText: widget.hintText,
-        hintStyle: Styles.captionRegular.copyWith(
-          color: kGrey_100Color,
-        ),
-        filled: true,
-        fillColor: kWhiteColor,
-      ),
+      ],
     );
   }
 }
