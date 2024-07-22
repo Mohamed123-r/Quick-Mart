4ca26558a28c331cab43446360696b7def41b232
mohamed <mezoattalla123@gmail.com>
2024-07-22 22:48:35 +0300

    Add optional width parameter to button widgets
    Enhance `PrimaryBottom` and `SecondaryBottom` classes by introducing an optional `width` parameter. Adjust the `minWidth` of the buttons to use this parameter, providing more flexibility in button sizing.

--- lib/core/widgets/custom_buttons.dart
+++ lib/core/widgets/custom_buttons.dart
@@ -4,16 +4,17 @@ import 'package:quick_mart/core/utils/styles.dart';
 
 class PrimaryBottom extends StatelessWidget {
   const PrimaryBottom(
-      {super.key, required this.title, required this.onPressed});
+      {super.key, required this.title, required this.onPressed, this.width});
 
   final Function() onPressed;
   final String title;
+  final double? width;
 
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
       height: 60,
-      minWidth: double.infinity,
+      minWidth: width ?? double.infinity,
       color: kBlackColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12),
@@ -31,26 +32,26 @@ class PrimaryBottom extends StatelessWidget {
 
 class SecondaryBottom extends StatelessWidget {
   const SecondaryBottom(
-      {super.key, required this.title, required this.onPressed});
+      {super.key, required this.title, required this.onPressed, this.width});
 
   final Function() onPressed;
   final String title;
+  final double? width;
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: MaterialButton(
-        elevation: 0,
-        height: 60,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-          side: const BorderSide(
-            color: kGrey_50Color,
-          ),
+    return MaterialButton(
+      elevation: 0,
+      height: 60,
+      minWidth: width ?? double.infinity,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+        side: const BorderSide(
+          color: kGrey_50Color,
         ),
-        onPressed: onPressed,
-        child: Text(title, style: Styles.button2SemiBold),
       ),
+      onPressed: onPressed,
+      child: Text(title, style: Styles.button2SemiBold),
     );
   }
 }
