c94550e267c7bfa4b5278b62e678e34eee7c4fb0
mohamed <mezoattalla123@gmail.com>
2024-07-23 22:34:13 +0300

    Add new routes for authentication and onboarding views in AppRouter

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:quick_mart/features/Authentication/login/presentation/view/log_in_view.dart';
+import 'package:quick_mart/features/Authentication/signup/presentation/view/sign_up_view.dart';
+import 'package:quick_mart/features/home/presentation/view/home_view.dart';
+import 'package:quick_mart/features/onboarding/presentation/view/onboarding_view.dart';
+import 'package:quick_mart/features/splash/presentation/view/splash_view.dart';
 
 abstract class AppRouter {
   static const kSplash = '/';
@@ -12,6 +17,29 @@ abstract class AppRouter {
 
   static Route<Object?> router(RouteSettings settings) {
     switch (settings.name) {
+      case kSplash:
+        return MaterialPageRoute(builder: (context) {
+          return const SplashView(); //replace this Scaffold with a Splash Screen
+        });
+      case kOnBoarding:
+        return MaterialPageRoute(builder: (context) {
+          return const OnboardingView(); //replace this Scaffold with a on boarding Screen
+        });
+
+      case kLogInView:
+        return MaterialPageRoute(builder: (context) {
+          return const LogInView(); //replace this Scaffold with a login Screen
+        });
+
+      case kSignUpView:
+        return MaterialPageRoute(builder: (context) {
+          return const SignUpView(); //replace this Scaffold with a login Screen
+        });
+
+      case kHomeView:
+        return MaterialPageRoute(builder: (context) {
+          return const HomeView(); //replace this Scaffold with a login Screen
+        });
 
       default:
         return MaterialPageRoute(builder: (context) {
