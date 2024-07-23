556c3e449417c0e74d162f7b0b171262bb8ca8a1
mohamed <mezoattalla123@gmail.com>
2024-07-23 23:13:28 +0300

    Add input fields section for signup form in Authentication module

---
+++ lib/features/Authentication/signup/presentation/view/widgets/input_fields_section.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart/core/function/validator.dart';
+import 'package:quick_mart/core/widgets/custom_input_field.dart';
+
+class InputFieldsSection extends StatelessWidget {
+  const InputFieldsSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomInputField(
+          hintText: 'Enter your Name',
+          title: 'Full Name',
+          keyboardType: TextInputType.text,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter the name';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        const CustomInputField(
+          hintText: 'Enter your Email',
+          title: 'Email',
+          keyboardType: TextInputType.emailAddress,
+          validator: validatorOfEmail,
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        const CustomInputField(
+          hintText: 'Enter your Password',
+          title: 'Password',
+          keyboardType: TextInputType.emailAddress,
+          validator: validatorOfPassword,
+        ),
+      ],
+    );
+  }
+}
