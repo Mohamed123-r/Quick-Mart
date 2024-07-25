49e8ad148921a50383fca87a601e085d0a2bf9f4
mohamed <mezoattalla123@gmail.com>
2024-07-25 15:59:08 +0300

    Add input fields section to login view widget
    Introduce a new `InputFieldsSectionFromLoginView` widget, containing email and password fields with validation. This enhances the login interface by providing user-friendly input options.

---
+++ lib/features/Authentication/login/presentation/view/widgets/input_fields_section_from_login_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart/core/function/validator.dart';
+import 'package:quick_mart/core/widgets/custom_input_field.dart';
+import 'package:quick_mart/core/widgets/custom_password_input_field.dart';
+
+class InputFieldsSectionFromLoginView extends StatelessWidget {
+  const InputFieldsSectionFromLoginView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomInputField(
+          hintText: 'Enter your Email',
+          title: 'Email',
+          keyboardType: TextInputType.emailAddress,
+          validator: validatorOfEmail,
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        CustomPasswordInputField(),
+      ],
+    );
+  }
+}
