6e81d98fb4c05b98e789e983dc943647df436839
mohamed <mezoattalla123@gmail.com>
2024-07-25 15:58:52 +0300

    Add custom password input field widget
    Create a new `CustomPasswordInputField` widget that includes visibility toggling for the password, a hint, and validation functionality.

---
+++ lib/core/widgets/custom_password_input_field.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart/core/function/validator.dart';
+import 'package:quick_mart/core/widgets/custom_input_field.dart';
+
+class CustomPasswordInputField extends StatefulWidget {
+  const CustomPasswordInputField({super.key});
+
+  @override
+  State<CustomPasswordInputField> createState() =>
+      _CustomPasswordInputFieldState();
+}
+
+class _CustomPasswordInputFieldState extends State<CustomPasswordInputField> {
+  bool obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomInputField(
+      hintText: 'Enter your Password',
+      title: 'Password',
+      keyboardType: TextInputType.emailAddress,
+      obscureText: obscureText,
+      suffixIcon: IconButton(
+        icon: obscureText
+            ? const Icon(Icons.remove_red_eye_outlined)
+            : const Icon(Icons.visibility_off_outlined),
+        onPressed: () {
+          setState(() {
+            obscureText = !obscureText;
+          });
+        },
+      ),
+      validator: validatorOfPassword,
+    );
+  }
+}
