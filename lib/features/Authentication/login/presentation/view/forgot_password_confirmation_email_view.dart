b16df2313530ed3ed878fd5aa93b48782628d035
mohamed <mezoattalla123@gmail.com>
2024-07-27 23:06:22 +0300

    Enhance ForgotPasswordConfirmationEmailView layout and styling
    - Add custom app bar with title and large format
    - Set background color to kWhiteColor
    - Implement structured body layout with divider and confirmation email view

--- lib/features/Authentication/login/presentation/view/forgot_password_confirmation_email_view.dart
+++ lib/features/Authentication/login/presentation/view/forgot_password_confirmation_email_view.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/widgets/custom_app_bar.dart';
+import 'package:quick_mart/features/Authentication/login/presentation/view/widgets/forgot_password_confirmation_email_view_body.dart';
 
 class ForgotPasswordConfirmationEmailView extends StatelessWidget {
   const ForgotPasswordConfirmationEmailView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: kWhiteColor,
+      appBar: customAppBar(
+        context: context,
+        isLarge: true,
+        title: "Forgot Password",
+      ),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Divider(
+            height: 0.5,
+          ),
+          SizedBox(height: 12),
+          ForgotPasswordConfirmationEmailViewBody(),
+        ],
+      ),
+    );
   }
 }
