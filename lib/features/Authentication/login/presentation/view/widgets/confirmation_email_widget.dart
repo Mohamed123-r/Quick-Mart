bdcf23ea70929ac3ee1484b771387fa192bdac98
mohamed <mezoattalla123@gmail.com>
2024-07-27 23:05:38 +0300

    Add confirmation email widget for user verification
    Create a new `ConfirmationEmailWidget` class that allows users to enter their email address for verification. Implement layout with appropriate styles and a custom input field.

---
+++ lib/features/Authentication/login/presentation/view/widgets/confirmation_email_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/function/validator.dart';
+import 'package:quick_mart/core/utils/styles.dart';
+import 'package:quick_mart/core/widgets/custom_input_field.dart';
+
+class ConfirmationEmailWidget extends StatelessWidget {
+  const ConfirmationEmailWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 12,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Confirmation Email",
+              style: Styles.heading2Bold,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              "Enter your email address for verification.",
+              style: Styles.body2Regular.copyWith(
+                color: kGrey_150Color,
+              ),
+            ),
+            const SizedBox(height: 16),
+          ],
+        ),
+        const CustomInputField(
+          hintText: 'Enter your Email',
+          title: 'Email',
+          keyboardType: TextInputType.emailAddress,
+          validator: validatorOfEmail,
+        ),
+      ],
+    );
+  }
+}
