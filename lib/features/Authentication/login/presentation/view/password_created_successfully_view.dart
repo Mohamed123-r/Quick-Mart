d24c185bfd53e5b6274046b697590e5db5a34e61
mohamed <mezoattalla123@gmail.com>
2024-07-28 01:32:08 +0300

    Refactor password created view to use Scaffold and enhance UI elements
    - Replace Placeholder with Scaffold for better structure
    - Add background color and safe area
    - Introduce padding and a column layout with image, headline, and button widgets
    - Improve overall visual design using custom constants and assets

--- lib/features/Authentication/login/presentation/view/password_created_successfully_view.dart
+++ lib/features/Authentication/login/presentation/view/password_created_successfully_view.dart
@@ -1,10 +1,43 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/utils/assets.dart';
+import 'package:quick_mart/features/Authentication/login/presentation/view/widgets/button_from_password_created_successfully_view.dart';
+import 'package:quick_mart/features/Authentication/login/presentation/view/widgets/headline_and_text_from_password_created_successfully_view.dart';
 
 class PasswordCreatedSuccessfullyView extends StatelessWidget {
   const PasswordCreatedSuccessfullyView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: kWhiteColor,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: kCyan_50Color,
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    child: Image.asset(
+                      Assets.passwordSuccessfully,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                const HeadlineAndTextFromPasswordCreatedSuccessfullyView(),
+                const SizedBox(
+                  height: 16,
+                ),
+                const ButtonFromPasswordCreatedSuccessfullyView(),
+              ],
+            ),
+          ),
+        ));
   }
 }
