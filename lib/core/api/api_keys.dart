d93344055ec6c8e1c05908cb1df73a766531d744
mohamed <mezoattalla123@gmail.com>
2024-08-01 19:56:06 +0300

    Add additional API key constants to ApiKeys class

--- lib/core/api/api_keys.dart
+++ lib/core/api/api_keys.dart
@@ -3,4 +3,10 @@ class ApiKeys {
   static String status = "status";
   static String message = "message";
   static String data = "data";
+  static String name = "name";
+  static String phone = "phone";
+  static String id = "id";
+  static String image = "image";
+  static String email = "email";
+  static String password = "password";
 }
