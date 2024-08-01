b18fd37889569b25e0b42b650fc0e94059801207
mohamed <mezoattalla123@gmail.com>
2024-08-01 23:21:06 +0300

    Add user authentication middleware for improved security

--- lib/features/Authentication/login/presentation/view/log_in_view.dart
+++ lib/features/Authentication/login/presentation/view/log_in_view.dart
@@ -1,20 +1,31 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/api/dio_consumer.dart';
 import 'package:quick_mart/core/widgets/logo_of_app.dart';
 import 'package:quick_mart/features/Authentication/login/presentation/view/widgets/log_in_view_body.dart';
+import 'package:quick_mart/features/authentication/login/presentation/manage/cubits/log_in_cubit.dart';
 
 class LogInView extends StatelessWidget {
   const LogInView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kWhiteColor,
-      appBar: AppBar(
-        backgroundColor: kWhiteColor,
-        title: const LogoOfApp(),
+    return BlocProvider(
+      create: (context) => LogInCubit(
+        DioConsumer(
+          dio: Dio(),
+        ),
       ),
-      body: LogInViewBody(),
+      child: Scaffold(
+        backgroundColor: kWhiteColor,
+        appBar: AppBar(
+          backgroundColor: kWhiteColor,
+          title: const LogoOfApp(),
+        ),
+        body: LogInViewBody(),
+      ),
     );
   }
 }
