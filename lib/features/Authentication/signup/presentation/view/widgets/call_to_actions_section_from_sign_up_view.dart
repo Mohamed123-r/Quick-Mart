85a25f99c30adf90c9ba83503ec0a7ce292d4017
mohamed <mezoattalla123@gmail.com>
2024-08-01 20:11:26 +0300

    Add new feature for user authentication and improve error handling

--- lib/features/Authentication/signup/presentation/view/widgets/call_to_actions_section_from_sign_up_view.dart
+++ lib/features/Authentication/signup/presentation/view/widgets/call_to_actions_section_from_sign_up_view.dart
@@ -1,42 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:quick_mart/core/utils/app_router.dart';
-import 'package:quick_mart/core/utils/assets.dart';
 import 'package:quick_mart/core/widgets/custom_buttons.dart';
-import 'package:quick_mart/core/widgets/custom_toast.dart';
 
 class CallToActionsSectionFromSignUpView extends StatelessWidget {
   const CallToActionsSectionFromSignUpView({
     super.key,
+    required this.primaryBottom,
+    required this.secondaryBottom,
+    this.isLoading = true,
   });
+
+  final Function() primaryBottom;
+  final Function() secondaryBottom;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         PrimaryBottom(
+          isLoading: isLoading,
           title: 'Create Account',
-          onPressed: () {
-            Navigator.push(
-              context,
-              AppRouter.router(
-                const RouteSettings(name: AppRouter.kSignupEmailVerification),
-              ),
-            );
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => CustomToast(
-                image: Assets.toastSuccessIcon,
-                title:
-                    '6-digit Verification code has been send to your email address.',
-              ),
-            ));
-          },
+          onPressed: primaryBottom,
         ),
         const SizedBox(
           height: 16,
         ),
         SecondaryBottom(
           title: 'SignUp with Google',
-          onPressed: () {},
+          onPressed: secondaryBottom,
         ),
       ],
     );
--- lib/features/Authentication/signup/presentation/view/widgets/input_fields_section_from_sign_up_view.dart
+++ lib/features/Authentication/signup/presentation/view/widgets/input_fields_section_from_sign_up_view.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:quick_mart/core/function/validator.dart';
 import 'package:quick_mart/core/widgets/custom_input_field.dart';
+import 'package:quick_mart/core/widgets/custom_password_input_field.dart';
 
 class InputFieldsSectionFromSignUpView extends StatelessWidget {
   const InputFieldsSectionFromSignUpView({
     super.key,
+    required this.nameController,
+    required this.emailController,
+    required this.passwordController,
+    required this.phoneController,
   });
+
+  final TextEditingController nameController;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final TextEditingController phoneController;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         CustomInputField(
+          controller: nameController,
           hintText: 'Enter your Name',
           title: 'Full Name',
           keyboardType: TextInputType.text,
@@ -25,7 +36,29 @@ class InputFieldsSectionFromSignUpView extends StatelessWidget {
         const SizedBox(
           height: 16,
         ),
-        const CustomInputField(
+        CustomInputField(
+          controller: phoneController,
+          hintText: 'Enter your Phone Number',
+          title: 'Phone Number',
+          keyboardType: TextInputType.number,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter the phone number';
+            }
+            if (value.length != 11) {
+              return 'Please enter a valid phone number';
+            }
+            if (!value.startsWith('01')) {
+              return 'Please enter a valid phone number';
+            }
+            return null;
+          },
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        CustomInputField(
+          controller: emailController,
           hintText: 'Enter your Email',
           title: 'Email',
           keyboardType: TextInputType.emailAddress,
@@ -34,16 +67,9 @@ class InputFieldsSectionFromSignUpView extends StatelessWidget {
         const SizedBox(
           height: 16,
         ),
-        CustomInputField(
-          hintText: 'Enter your Password',
+        CustomPasswordInputField(
           title: 'Password',
-          keyboardType: TextInputType.emailAddress,
-          obscureText: true,
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.visibility),
-            onPressed: () {},
-          ),
-          validator: validatorOfPassword,
+          controller: passwordController,
         ),
       ],
     );
