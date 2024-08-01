c33f3055d47f566964f49ed66db17b4ce00ff31c
mohamed <mezoattalla123@gmail.com>
2024-08-01 16:02:51 +0300

    Refactor CustomToast to simplify structure and remove StatefulWidget
    - Convert CustomToast from StatefulWidget to a simple class
    - Remove unnecessary state management and initState method
    - Pass BuildContext and FToast directly to the showToast method
    - Update toast display logic for improved clarity and efficiency

--- lib/core/widgets/custom_toast.dart
+++ lib/core/widgets/custom_toast.dart
@@ -4,55 +4,45 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:quick_mart/constants.dart';
 import 'package:quick_mart/core/utils/styles.dart';
 
-class CustomToast extends StatefulWidget {
-  const CustomToast({super.key, required this.image, required this.title});
+class CustomToast {
+  const CustomToast(
+      {required this.context,
+      required this.fToast,
+      required this.image,
+      required this.title});
 
   final String image;
   final String title;
+  final BuildContext context;
+  final FToast fToast;
 
-  @override
-  State<CustomToast> createState() => _ToastContextState();
-}
-
-class _ToastContextState extends State<CustomToast> {
-  late FToast fToast;
-
-  @override
-  void initState() {
-    super.initState();
-    fToast = FToast();
-    // if you want to use context from globally instead of content we need to pass navigatorKey.currentContext!
-    fToast.init(context);
-  }
-
-  _showToast() {
-    Widget toast = Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16),
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12.0),
-        border: Border.all(
+  showToast() {
+    Widget toast = Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width - 32,
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0),
+          border: Border.all(
+            color: kGrey_100Color,
+            width: 2,
+          ),
           color: kGrey_50Color,
-          width: 2,
         ),
-        color: kWhiteColor,
-      ),
-      child: Row(
-        children: [
-          SvgPicture.asset(
-            widget.image,
-          ),
-          const SizedBox(
-            width: 8,
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width - (88),
-            child: Text(
-              widget.title,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SvgPicture.asset(
+              image,
+            ),
+            Text(
+              title,
+              textAlign: TextAlign.end,
               style: Styles.captionSemiBold,
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
 
@@ -66,19 +56,5 @@ class _ToastContextState extends State<CustomToast> {
             child: child,
           );
         });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Future.delayed(
-      const Duration(microseconds: 1),
-      () {
-        _showToast();
-        Navigator.pop(context);
-      },
-    );
-    return Scaffold(
-      body: Center(child: Container()),
-    );
   }
 }
