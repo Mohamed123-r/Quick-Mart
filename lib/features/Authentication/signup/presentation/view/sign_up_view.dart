eeac2f10b35e5da86e89e701c8756bb22a25a362
mohamed <mezoattalla123@gmail.com>
2024-08-01 20:11:38 +0300

    Add new feature for user authentication and improve error handling

--- lib/features/Authentication/signup/presentation/view/sign_up_view.dart
+++ lib/features/Authentication/signup/presentation/view/sign_up_view.dart
@@ -1,20 +1,31 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/api/dio_consumer.dart';
 import 'package:quick_mart/core/widgets/logo_of_app.dart';
 import 'package:quick_mart/features/Authentication/signup/presentation/view/widgets/sign_up_view_body.dart';
+import 'package:quick_mart/features/authentication/signup/presentation/manage/cubits/sign_up_cubit.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kWhiteColor,
-      appBar: AppBar(
-        backgroundColor: kWhiteColor,
-        title: const LogoOfApp(),
+    return BlocProvider(
+      create: (context) => SignUpCubit(
+        DioConsumer(
+          dio: Dio(),
+        ),
       ),
-      body: const SignUpViewBody(),
+      child: Scaffold(
+        backgroundColor: kWhiteColor,
+        appBar: AppBar(
+          backgroundColor: kWhiteColor,
+          title: const LogoOfApp(),
+        ),
+        body: SignUpViewBody(),
+      ),
     );
   }
 }
