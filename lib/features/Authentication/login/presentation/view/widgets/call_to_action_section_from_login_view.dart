b846c8ae7cb86a4ea9a7a128291bf4adaefa31bf
mohamed <mezoattalla123@gmail.com>
2024-08-01 23:20:37 +0300

    Sure! Please provide the git diff you would like me to write a commit message for.

--- lib/features/Authentication/login/presentation/view/widgets/call_to_action_section_from_login_view.dart
+++ lib/features/Authentication/login/presentation/view/widgets/call_to_action_section_from_login_view.dart
@@ -4,22 +4,30 @@ import 'package:quick_mart/core/widgets/custom_buttons.dart';
 class CallToActionSectionFromLoginView extends StatelessWidget {
   const CallToActionSectionFromLoginView({
     super.key,
+    required this.isLoading,
+    required this.primaryBottom,
+    required this.secondaryBottom,
   });
+
+  final bool isLoading;
+  final Function() primaryBottom;
+  final Function() secondaryBottom;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         PrimaryBottom(
+          isLoading: isLoading,
           title: 'Login',
-          onPressed: () {},
+          onPressed: primaryBottom,
         ),
         const SizedBox(
           height: 16,
         ),
         SecondaryBottom(
           title: 'Login with Google',
-          onPressed: () {},
+          onPressed: secondaryBottom,
         ),
       ],
     );
