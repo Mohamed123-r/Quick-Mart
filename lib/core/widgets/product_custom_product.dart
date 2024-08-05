aac8535b4bfdf9a2b92960661ded2ca99ae3b6f2
mohamed <mezoattalla123@gmail.com>
2024-08-05 22:48:11 +0300

    Add ProductCustomProduct widget with enhanced layout and styling
    - Implement ProductCustomProduct as a stateless widget.
    - Use Stack for image and wishlist positioning.
    - Redesign color selection layout with Positioned widgets.
    - Display product name and price with styles from the Styles utility.

---
+++ lib/core/widgets/product_custom_product.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/utils/assets.dart';
+import 'package:quick_mart/core/utils/styles.dart';
+import 'package:quick_mart/core/widgets/product_custom_colors.dart';
+import 'package:quick_mart/core/widgets/product_custom_wishlist.dart';
+
+class ProductCustomProduct extends StatelessWidget {
+  const ProductCustomProduct({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Stack(children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Image.asset(
+              Assets.productTest,
+            ),
+          ),
+          const Positioned(
+            top: 8,
+            right: 8,
+            child: ProductCustomWishlist(),
+          ),
+        ]),
+        const SizedBox(height: 8),
+        const Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Positioned(
+              left: 32,
+              child: ProductCustomColors(
+                color: kRedColor,
+                isSelected: false,
+              ),
+            ),
+            Positioned(
+              left: 16,
+              child: ProductCustomColors(
+                color: kCyanColor,
+                isSelected: false,
+              ),
+            ),
+            ProductCustomColors(
+              color: kBlackColor,
+              isSelected: true,
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+        const SizedBox(height: 8),
+        const Text(
+          "Product Name",
+          style: Styles.body2Medium,
+        ),
+        const Text(
+          "\$ 19.99",
+          style: Styles.captionSemiBold,
+        ),
+        Text(
+          "\$ 19.99",
+          style: Styles.overLineRegular.copyWith(
+            color: kGrey_150Color,
+            decoration: TextDecoration.lineThrough,
+          ),
+        ),
+      ],
+    );
+  }
+}
