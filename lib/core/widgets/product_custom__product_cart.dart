f84bc16222bbb63adb4f428fb92d9e7c4d82047a
mohamed <mezoattalla123@gmail.com>
2024-08-06 00:06:43 +0300

    Improve product cart widget layout and add trash icon
    - Adjust image dimensions from 120 to 130.
    - Use `const` for `SizedBox` and `Expanded` widgets to enhance performance.
    - Remove unnecessary `SizedBox` for spacing.
    - Update the row containing the quantity and replace placeholder with a trash icon using `SvgPicture`.

---
+++ lib/core/widgets/product_custom__product_cart.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/utils/assets.dart';
+import 'package:quick_mart/core/utils/styles.dart';
+import 'package:quick_mart/core/widgets/product_custom_quantity.dart';
+
+class ProductCustomProductCart extends StatelessWidget {
+  const ProductCustomProductCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset(
+          Assets.productTest,
+          width: 130,
+          height: 130,
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        Expanded(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Expanded(
+                    child: Text(
+                      "Product Name mind mdinfon asokmcoisn nmsikocni smondoi fddr fd dc",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.body2Medium,
+                    ),
+                  ),
+                  Checkbox(
+                    value: true,
+                    onChanged: (value) {},
+                    activeColor: kCyanColor,
+                  ),
+                ],
+              ),
+              const Text(
+                "\$ 19.99",
+                style: Styles.captionSemiBold,
+              ),
+              Text(
+                "\$ 19.99",
+                style: Styles.overLineRegular.copyWith(
+                  color: kGrey_150Color,
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const ProductCustomQuantity(),
+                  SvgPicture.asset(
+                    Assets.trashIcon,
+                  ),
+                ],
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
