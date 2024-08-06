5249977d3436035644b9bb7817d8206b376d44d6
mohamed <mezoattalla123@gmail.com>
2024-08-06 21:27:32 +0300

    Add product custom stepper widget file

---
+++ lib/core/widgets/product_custom_stepper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductCustomStepper extends StatelessWidget {
+  const ProductCustomStepper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
