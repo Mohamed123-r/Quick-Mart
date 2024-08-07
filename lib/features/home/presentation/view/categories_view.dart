985304bf259ed05ee57695e723fd7c9e61dfd6da
mohamed <mezoattalla123@gmail.com>
2024-08-07 03:24:07 +0300

    Add CategoriesView and CategoriesViewBody components
    Create a new CategoriesView widget with a custom app bar and a grid-based layout. Implement CategoriesViewBody to display product categories using a GridView, enhancing the home feature's UI.

---
+++ lib/features/home/presentation/view/categories_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/widgets/custom_app_bar.dart';
+import 'package:quick_mart/features/home/presentation/view/widgets/categories_view_body.dart';
+
+class CategoriesView extends StatelessWidget {
+  const CategoriesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kWhiteColor,
+      appBar: customAppBar(
+        context: context,
+        isLarge: true,
+        title: 'Categories',
+      ),
+      body: const CategoriesViewBody(),
+    );
+  }
+}
---
+++ lib/features/home/presentation/view/widgets/categories_view_body.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart/core/utils/assets.dart';
+import 'package:quick_mart/core/widgets/product_custom_category.dart';
+
+class CategoriesViewBody extends StatelessWidget {
+  const CategoriesViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+        childAspectRatio: 1.6,
+        children: List.generate(
+          10,
+          (index) => InkWell(
+            onTap: () {},
+            borderRadius: BorderRadius.circular(16),
+            child: ProductCustomCategory(
+              title: ' Electronics',
+              icon: Assets.heart2Icon,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
