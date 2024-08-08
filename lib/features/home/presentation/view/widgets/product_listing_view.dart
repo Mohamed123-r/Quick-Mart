e62ec2c2bd8b9b16ad26af0fb78df06c16ace6a0
mohamed <mezoattalla123@gmail.com>
2024-08-08 19:38:59 +0300

    Add custom app bar to product listing view in Flutter application

--- lib/features/home/presentation/view/widgets/product_listing_view.dart
+++ lib/features/home/presentation/view/widgets/product_listing_view.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:quick_mart/core/widgets/custom_app_bar.dart';
 
 class ProductListingView extends StatelessWidget {
   const ProductListingView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: customAppBar(
+        context: context,
+        title: 'Product Listing',
+        isLarge: true,
+        actions: const [
+          Icon(
+            Icons.more_vert,
+          ),
+        ],
+      ),
+    );
   }
 }
