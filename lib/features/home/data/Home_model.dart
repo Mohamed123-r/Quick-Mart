f420c2b37ba4bbe6571969e7d1f57fe107f59d81
mohamed <mezoattalla123@gmail.com>
2024-08-11 20:09:10 +0300

    Implement HomeCubit and HomeState classes for state management

---
+++ lib/features/home/data/Home_model.dart
@@ -0,0 +1,29 @@
+import 'data.dart';
+
+class HomeModel {
+  HomeModel({
+    this.status,
+    this.message,
+    this.data,
+  });
+
+  HomeModel.fromJson(dynamic json) {
+    status = json['status'];
+    message = json['message'];
+    data = json['data'];
+  }
+
+  bool? status;
+  dynamic message;
+  Data? data;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['status'] = status;
+    map['message'] = message;
+    if (data != null) {
+      map['data'] = data?.toJson();
+    }
+    return map;
+  }
+}
