a22174e8a2619f477931947349bd0be5dda81784
mohamed <mezoattalla123@gmail.com>
2024-08-11 20:09:17 +0300

    Add API integration and state management to HomeCubit
    Implement API consumer in HomeCubit to fetch home data. Introduce loading, success, and failure states in HomeState for better state management.

--- lib/features/home/presentation/manage/cubits/home_cubit.dart
+++ lib/features/home/presentation/manage/cubits/home_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:quick_mart/core/api/api_consumer.dart';
+import 'package:quick_mart/core/api/end_point.dart';
+import 'package:quick_mart/features/home/data/Home_model.dart';
 
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeInitial());
+  HomeCubit(this.api) : super(HomeInitial());
+
+  final ApiConsumer api;
+  HomeModel? homeModel;
+
+  getHomeData() async {
+    emit(HomeLoading());
+
+    var response = await api.get(EndPoint.home);
+
+    homeModel = HomeModel.fromJson(response);
+    print(homeModel!.data!.banners![0].image);
+  }
 }
--- lib/features/home/presentation/manage/cubits/home_state.dart
+++ lib/features/home/presentation/manage/cubits/home_state.dart
@@ -4,3 +4,13 @@ part of 'home_cubit.dart';
 sealed class HomeState {}
 
 final class HomeInitial extends HomeState {}
+
+final class HomeLoading extends HomeState {}
+
+final class HomeSuccess extends HomeState {}
+
+final class HomeFailed extends HomeState {
+  final String error;
+
+  HomeFailed({required this.error});
+}
