dc90de2af78dd03b8f17e0822ac261b5b7687219
mohamed <mezoattalla123@gmail.com>
2024-08-14 23:47:09 +0300

    Add 'categories' endpoint to EndPoint class

--- lib/features/home/presentation/manage/cubits/categories_cubit.dart
+++ lib/features/home/presentation/manage/cubits/categories_cubit.dart
@@ -1,8 +1,37 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:quick_mart/core/api/api_consumer.dart';
+import 'package:quick_mart/core/api/end_point.dart';
+import 'package:quick_mart/core/errors/exceptions.dart';
 
 part 'categories_state.dart';
 
 class CategoriesCubit extends Cubit<CategoriesState> {
-  CategoriesCubit() : super(CategoriesInitial());
+  CategoriesCubit(this.api) : super(CategoriesInitial());
+  final ApiConsumer api;
+
+  Map<String, dynamic> data = {};
+  List? categoriesNames;
+
+  getCategories() async {
+    emit(CategoriesLoading());
+
+    try {
+      Map<String, dynamic> response = await api.get(EndPoint.categories);
+      log(response.toString());
+
+      for (int i = 0; i < response['data']['data'].length; i++) {
+        data.addAll(response['data']['data'][i]);
+        categoriesNames!.add(response['data']['data'][i]['name']);
+      }
+      log(data.toString());
+      log(categoriesNames.toString());
+    } on ServerException catch (e) {
+      emit(
+        CategoriesFailed(e.errorModel.message!),
+      );
+    }
+  }
 }
--- lib/features/home/presentation/manage/cubits/categories_state.dart
+++ lib/features/home/presentation/manage/cubits/categories_state.dart
@@ -4,3 +4,13 @@ part of 'categories_cubit.dart';
 sealed class CategoriesState {}
 
 final class CategoriesInitial extends CategoriesState {}
+
+final class CategoriesLoading extends CategoriesState {}
+
+final class CategoriesSuccess extends CategoriesState {}
+
+final class CategoriesFailed extends CategoriesState {
+  final String error;
+
+  CategoriesFailed(this.error);
+}
