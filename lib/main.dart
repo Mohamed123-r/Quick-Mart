7b15614b84e9c6c12ed49ed63931e92ea0e7cac8
mohamed <mezoattalla123@gmail.com>
2024-08-14 23:47:32 +0300

    Update import and replace HomeCubit with CategoriesCubit in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart/constants.dart';
 import 'package:quick_mart/core/api/dio_consumer.dart';
 import 'package:quick_mart/core/database/cache/cache_helper.dart';
-import 'package:quick_mart/features/home/presentation/manage/cubits/home_cubit.dart';
+import 'package:quick_mart/features/home/presentation/manage/cubits/categories_cubit.dart';
 import 'package:quick_mart/features/splash/presentation/view/splash_view.dart';
 
 Future<void> main() async {
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => HomeCubit(
+      create: (context) => CategoriesCubit(
         DioConsumer(
           dio: Dio(),
         ),
