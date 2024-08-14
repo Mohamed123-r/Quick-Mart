460066b8dc7df8afd3eb1093a1cffbd696b1fe76
mohamed <mezoattalla123@gmail.com>
2024-08-14 23:47:22 +0300

    Remove token assignment in ApiInterceptor on request header

--- lib/core/api/api_interceptor.dart
+++ lib/core/api/api_interceptor.dart
@@ -1,14 +1,10 @@
 import 'package:dio/dio.dart';
-import 'package:quick_mart/core/api/api_keys.dart';
-import '../database/cache/cache_helper.dart';
 
 
 class ApiInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     super.onRequest(options, handler);
-    options.headers['token'] = CacheHelper().getData(key: ApiKeys.token) != null
-        ? 'FOODAPI ${CacheHelper().getData(key: ApiKeys.token)}'
-        : null;
+    options.headers['token'] = null;
   }
 }
