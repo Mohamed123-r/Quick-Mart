12c0ce8d1fc9fad56313add5f288cb510efb832e
mohamed <mezoattalla123@gmail.com>
2024-08-14 23:47:57 +0300

    Add Bloc support to CategoriesViewBody widget
    Integrate CategoriesCubit in the CategoriesViewBody to manage state and update the category list dynamically. Adjust the GridView to display categories based on the current state and reduce the number of generated categories from 10 to 4.

--- lib/features/home/presentation/view/widgets/categories_view_body.dart
+++ lib/features/home/presentation/view/widgets/categories_view_body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart/core/utils/app_router.dart';
 import 'package:quick_mart/core/utils/assets.dart';
 import 'package:quick_mart/core/widgets/product_custom_category.dart';
+import 'package:quick_mart/features/home/presentation/manage/cubits/categories_cubit.dart';
 
 class CategoriesViewBody extends StatelessWidget {
   const CategoriesViewBody({
@@ -10,32 +12,37 @@ class CategoriesViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: GridView.count(
-        crossAxisCount: 2,
-        crossAxisSpacing: 8,
-        mainAxisSpacing: 8,
-        childAspectRatio: 1.6,
-        children: List.generate(
-          10,
-          (index) => InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                AppRouter.router(
-                  const RouteSettings(name: AppRouter.kSubCategoriesView),
+    return BlocBuilder<CategoriesCubit, CategoriesState>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
+            childAspectRatio: 1.6,
+            children: List.generate(
+              4,
+              (index) => InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    AppRouter.router(
+                      const RouteSettings(name: AppRouter.kSubCategoriesView),
+                    ),
+                  );
+                },
+                borderRadius: BorderRadius.circular(16),
+                child: ProductCustomCategory(
+                  title:
+                      context.read<CategoriesCubit>().categoriesNames![index],
+                  icon: Assets.heart2Icon,
                 ),
-              );
-            },
-            borderRadius: BorderRadius.circular(16),
-            child: ProductCustomCategory(
-              title: ' Electronics',
-              icon: Assets.heart2Icon,
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
