05d20d614f4a33102345d0acce02a83afad45d1b
mohamed <mezoattalla123@gmail.com>
2024-08-16 16:39:36 +0300

    Refactor CategoriesSection to use Bloc for state management
    - Integrate `BlocBuilder` to manage loading states and display categories dynamically.
    - Remove commented-out code and streamline the layout.
    - Add loading indicator while categories are being fetched.

--- lib/features/home/presentation/view/widgets/categories_section_from_home_view.dart
+++ lib/features/home/presentation/view/widgets/categories_section_from_home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart/constants.dart';
 import 'package:quick_mart/core/utils/styles.dart';
+import 'package:quick_mart/core/widgets/product_custom_category.dart';
+import 'package:quick_mart/features/home/presentation/manage/cubits/categories_cubit/categories_cubit.dart';
 
 class CategoriesSectionFromHomeView extends StatelessWidget {
   const CategoriesSectionFromHomeView({
@@ -12,54 +15,67 @@ class CategoriesSectionFromHomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return BlocBuilder<CategoriesCubit, CategoriesState>(
+      builder: (context, state) {
+        return Column(
           children: [
-            const Text(
-              "Categories",
-              style: Styles.heading3Bold,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Categories",
+                  style: Styles.heading3Bold,
+                ),
+                InkWell(
+                  onTap: onTapOfSeeAll,
+                  borderRadius: BorderRadius.circular(24),
+                  child: Text(
+                    "See All",
+                    style: Styles.body2Medium.copyWith(color: kCyanColor),
+                  ),
+                ),
+              ],
             ),
-            InkWell(
-              onTap: onTapOfSeeAll,
-              borderRadius: BorderRadius.circular(24),
-              child: Text(
-                "See All",
-                style: Styles.body2Medium.copyWith(color: kCyanColor),
-              ),
+            const SizedBox(
+              height: 12,
             ),
+            state is CategoriesLoading
+                ? const SizedBox(
+                    height: 60,
+                    child: Center(
+                      child: CircularProgressIndicator(
+                        color: kCyanColor,
+                      ),
+                    ),
+                  )
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ProductCustomCategory(
+                        isSmall: true,
+                        title: dataOfCategories[0]['name'],
+                        icon: dataOfCategories[0]['image'],
+                      ),
+                      ProductCustomCategory(
+                        isSmall: true,
+                        title: dataOfCategories[1]['name'],
+                        icon: dataOfCategories[1]['image'],
+                      ),
+                      ProductCustomCategory(
+                        isSmall: true,
+                        title: dataOfCategories[2]['name'],
+                        icon: dataOfCategories[2]['image'],
+                      ),
+                      ProductCustomCategory(
+                        isSmall: true,
+                        title: dataOfCategories[3]['name'],
+                        icon: dataOfCategories[3]['image'],
+                      ),
+                    ],
+                  )
           ],
-        ),
-        const SizedBox(
-          height: 12,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // ProductCustomCategory(
-            //   isSmall: true,
-            //   title: dataOfCategories[3]['name'],
-            //   icon: dataOfCategories[0]['image'],
-            // ),
-            // ProductCustomCategory(
-            //   isSmall: true,
-            //   title: dataOfCategories[1]['name'],
-            //   icon: dataOfCategories[1]['image'],
-            // ),
-            // ProductCustomCategory(
-            //   isSmall: true,
-            //   title: dataOfCategories[2]['name'],
-            //   icon: dataOfCategories[2]['image'],
-            // ),
-            // ProductCustomCategory(
-            //   isSmall: true,
-            //   title: dataOfCategories[3]['name'],
-            //   icon: dataOfCategories[3]['image'],
-            // ),
-          ],
-        )
-      ],
+        );
+      },
     );
   }
 }
