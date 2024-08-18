d2015be930857e31b10a3ac27997ca89ef3aaaa2
mohamed <mezoattalla123@gmail.com>
2024-08-18 21:48:02 +0300

    Enhance CustomSearch widget and update SearchView layout
    - Add `onChanged` and `controller` parameters to `CustomSearch`.
    - Implement a custom app bar in `SearchView` with a logo and close button.
    - Set background color for the `SearchView` to improve UI consistency.

--- lib/core/widgets/custom_search.dart
+++ lib/core/widgets/custom_search.dart
@@ -4,11 +4,15 @@ import 'package:quick_mart/core/utils/assets.dart';
 import 'package:quick_mart/core/utils/styles.dart';
 
 class CustomSearch extends StatelessWidget {
-  const CustomSearch({super.key});
+  const CustomSearch({super.key, this.onChanged, this.controller});
 
+  final Function(String)? onChanged;
+  final TextEditingController? controller;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
+      onChanged: onChanged,
       cursorColor: Colors.green,
       textInputAction: TextInputAction.search,
       decoration: InputDecoration(
--- lib/features/search/presentation/view/search_view.dart
+++ lib/features/search/presentation/view/search_view.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/widgets/logo_of_app.dart';
+import 'package:quick_mart/features/search/presentation/view/widgets/search_view_body.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: kWhiteColor,
+      appBar: AppBar(
+        leadingWidth: 0,
+        backgroundColor: kWhiteColor,
+        leading: const SizedBox(),
+        title: const LogoOfApp(),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.close,
+            ),
+          ),
+        ],
+      ),
+      body: const SearchViewBody(),
+    );
   }
 }
