6d9f47261969c246e75176320da4677dfd75661b
mohamed <mezoattalla123@gmail.com>
2024-08-21 09:16:54 +0300

    Refactor product detail view to utilize Bloc for state management
    - Implement `BlocBuilder` for product details loading state in `product_detail_view_body.dart`.
    - Replace hardcoded image assets with dynamic images from product details data.
    - Integrate `BlocBuilder` in `section_of_he_name_and_description_from_product_details_view.dart` for category details.
    - Update text fields to display product name, price, and old price dynamically from the product details data.

--- lib/features/home/presentation/view/widgets/product_detail_view_body.dart
+++ lib/features/home/presentation/view/widgets/product_detail_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quick_mart/core/utils/assets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/features/home/presentation/manage/cubits/product_details_cubit/product_details_cubit.dart';
 import 'package:quick_mart/features/home/presentation/view/widgets/image_section_from_product_detail_view.dart';
 import 'package:quick_mart/features/home/presentation/view/widgets/product_details_section_from_product_detail_view.dart';
 
@@ -10,41 +12,39 @@ class ProductDetailViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(
-            height: 16,
-          ),
-          ImageSectionFromProductDetailView(
-            lengthItem: 5,
-            items: [
-              Image.asset(
-                Assets.productTest,
-                fit: BoxFit.fill,
-              ),
-              Image.asset(
-                Assets.productTest,
-                fit: BoxFit.fill,
-              ),
-              Image.asset(
-                Assets.productTest,
-                fit: BoxFit.fill,
-              ),
-              Image.asset(
-                Assets.productTest,
-                fit: BoxFit.fill,
-              ),
-              Image.asset(
-                Assets.productTest,
-                fit: BoxFit.fill,
-              ),
-            ],
-          ),
-          const ProductDetailsSectionFromProductDetailView(),
-        ],
-      ),
+    return BlocBuilder<ProductDetailsCubit, ProductDetailsState>(
+      builder: (context, state) {
+        //BlocProvider.of<ProductDetailsCubit>.getProductDetails());
+        return state is ProductDetailsLoading
+            ? const Center(
+                child: CircularProgressIndicator(
+                  color: kCyanColor,
+                ),
+              )
+            : SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    ImageSectionFromProductDetailView(
+                      lengthItem: dataOfProductDetails['images'].length,
+                      items: [
+                        for (int i = 0;
+                            i < dataOfProductDetails['images'].length;
+                            i++)
+                          Image.network(
+                            dataOfProductDetails['images'][i],
+                            // fit: BoxFit.fill,
+                          ),
+                      ],
+                    ),
+                    const ProductDetailsSectionFromProductDetailView(),
+                  ],
+                ),
+              );
+      },
     );
   }
 }
--- lib/features/home/presentation/view/widgets/section_of_he_name_and_description_from_product_details_view.dart
+++ lib/features/home/presentation/view/widgets/section_of_he_name_and_description_from_product_details_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart/constants.dart';
 import 'package:quick_mart/core/utils/styles.dart';
+import 'package:quick_mart/features/home/presentation/manage/cubits/categories_details_cubit/category_details_cubit.dart';
+import 'package:quick_mart/features/home/presentation/manage/cubits/product_details_cubit/product_details_cubit.dart';
 import 'package:readmore/readmore.dart';
 
 class SectionOfTheNameAndDescriptionFromProductDetailsView
@@ -11,52 +14,57 @@ class SectionOfTheNameAndDescriptionFromProductDetailsView
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return BlocBuilder<CategoryDetailsCubit, CategoryDetailsState>(
+      builder: (context, state) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Expanded(
-              child: Text(
-                'Loop Silicone Strong Magnetic watch',
-                style: Styles.heading3Bold,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            Column(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text('\$ 15.25', style: Styles.heading3Bold),
-                Text('\$ 15.25',
-                    style: Styles.body2Regular.copyWith(
-                      color: kGrey_150Color,
-                      decoration: TextDecoration.lineThrough,
-                    ))
+                Expanded(
+                  child: Text(
+                    dataOfProductDetails['name'] ?? '',
+                    style: Styles.heading3Bold,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Column(
+                  children: [
+                    Text('\$ ${dataOfProductDetails['price']}?? ' '',
+                        style: Styles.heading3Bold),
+                    Text('\$ ${dataOfProductDetails['old_price']}',
+                        style: Styles.body2Regular.copyWith(
+                          color: kGrey_150Color,
+                          decoration: TextDecoration.lineThrough,
+                        ))
+                  ],
+                )
               ],
-            )
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const Text(
+              'Product Details',
+              style: Styles.body1Medium,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            ReadMoreText(
+              dataOfProductDetails['description'] ?? '',
+              trimMode: TrimMode.Length,
+              trimLines: 5,
+              colorClickableText: kCyanColor,
+              style: Styles.body2Regular.copyWith(color: kGrey_150Color),
+              trimCollapsedText: 'Read more',
+              trimExpandedText: 'Read less',
+            ),
           ],
-        ),
-        const SizedBox(
-          height: 12,
-        ),
-        const Text(
-          'Product Details',
-          style: Styles.body1Medium,
-        ),
-        const SizedBox(
-          height: 8,
-        ),
-        ReadMoreText(
-          'Flutter is Google’s mobile UI open source framework to build high-quality  Flutter is Google’s mobile UI open source framework to build high-qualit Flutter is Google’s mobile UI open source framework to build high-qualityFlutter is Google’s mobile UI open source framework to build high-qualityFlutter is Google’s mobile UI open source framework to build high-qualityFlutter is Google’s mobile UI open source framework to build high-qualityyFlutter is Google’s mobile UI open source framework to build high-quality Flutter is Google’s mobile UI open source framework to build high-quality Flutter is Google’s mobile UI open source framework to build high-quality native (super fast) interfaces for iOS and Android apps with the unified codebase.',
-          trimMode: TrimMode.Length,
-          trimLines: 5,
-          colorClickableText: kCyanColor,
-          style: Styles.body2Regular.copyWith(color: kGrey_150Color),
-          trimCollapsedText: 'Read more',
-          trimExpandedText: 'Read less',
-        ),
-      ],
+        );
+      },
     );
   }
 }
