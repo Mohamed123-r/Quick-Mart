70b33acbd288745f7d6ba4fb9cb42e1725ca6f91
mohamed <mezoattalla123@gmail.com>
2024-09-02 16:06:54 +0300

    Refactor CustomDropDawn widget to use CountryStateCityPicker, remove unused properties, and update dependencies.

--- lib/core/widgets/custom_dropdawn.dart
+++ lib/core/widgets/custom_dropdawn.dart
@@ -1,13 +1,12 @@
-import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:country_state_city_pro/country_state_city_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:quick_mart/constants.dart';
 import 'package:quick_mart/core/utils/styles.dart';
 
 class CustomDropDawn extends StatefulWidget {
-  const CustomDropDawn({super.key, required this.items, required this.hint});
-
-  final List<String> items;
-  final String hint;
+  const CustomDropDawn({
+    super.key,
+  });
 
   @override
   State<CustomDropDawn> createState() => _CustomDropDawnState();
@@ -16,85 +15,47 @@ class CustomDropDawn extends StatefulWidget {
 class _CustomDropDawnState extends State<CustomDropDawn> {
   String? selectedValue;
   Color color = Colors.black12;
+  TextEditingController country = TextEditingController();
+  TextEditingController state = TextEditingController();
+  TextEditingController city = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField2<String>(
-      isExpanded: true,
-      decoration: InputDecoration(
-        // Add Horizontal padding using menuItemStyleData.padding so it matches
-        // the menu padding when button's width is not specified.
-        contentPadding: const EdgeInsets.symmetric(vertical: 18),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-          borderSide: const BorderSide(
-            color: kGrey_50Color,
+    return Theme(
+      data: ThemeData(
+        primaryColor: kCyanColor,
+      ),
+      child: CountryStateCityPicker(
+        country: country,
+        state: state,
+        city: city,
+        dialogColor: kWhiteColor,
+        textFieldDecoration: InputDecoration(
+          hintStyle: Styles.captionRegular.copyWith(color: kBlackColor),
+          suffixIcon: const Icon(
+            Icons.keyboard_arrow_down_outlined,
+          ),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 18, horizontal: 16),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: const BorderSide(
+              color: kGrey_50Color,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide(
+              color: color,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide(
+              color: color,
+            ),
           ),
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide(
-            color: color,
-          ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide(
-            color: color,
-          ),
-        ),
-      ),
-      hint: Text(
-        widget.hint,
-        style: Styles.captionRegular.copyWith(color: kBlackColor),
-      ),
-      items: widget.items
-          .map((item) => DropdownMenuItem<String>(
-                value: item,
-                child: Text(
-                  item,
-                  style: const TextStyle(
-                    fontSize: 14,
-                  ),
-                ),
-              ))
-          .toList(),
-      validator: (value) {
-        if (value == null) {
-          return 'Please select an item.';
-        }
-        return null;
-      },
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      onChanged: (value) {
-        //Do something when selected item is changed.
-        setState(() {
-          color = kBlueColor;
-        });
-      },
-      onSaved: (value) {
-        selectedValue = value.toString();
-      },
-      buttonStyleData: const ButtonStyleData(
-        padding: EdgeInsets.only(right: 12),
-      ),
-      iconStyleData: const IconStyleData(
-        icon: Icon(
-          Icons.keyboard_arrow_down_outlined,
-          color: kBlackColor,
-        ),
-        iconSize: 24,
-      ),
-      dropdownStyleData: DropdownStyleData(
-        decoration: BoxDecoration(
-          color: Colors.transparent,
-          boxShadow: const [],
-          border: Border.all(color: kGrey_50Color),
-          borderRadius: BorderRadius.circular(15),
-        ),
-      ),
-      menuItemStyleData: const MenuItemStyleData(
-        padding: EdgeInsets.symmetric(horizontal: 16),
       ),
     );
   }
