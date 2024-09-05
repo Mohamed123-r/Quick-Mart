f66e044cf86f064e590f22f66cfbf66555a11ca9
mohamed <mezoattalla123@gmail.com>
2024-09-05 22:22:40 +0300

    Add ChangePasswordView with custom app bar and body layout
    Implement ChangePasswordView widget to provide a UI for changing passwords, including a custom app bar and a placeholder for the body content.

--- lib/features/account/presentation/view/widgets/change_password_view.dart
+++ lib/features/account/presentation/view/widgets/change_password_view.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:quick_mart/constants.dart';
+import 'package:quick_mart/core/widgets/custom_app_bar.dart';
 
 class ChangePasswordView extends StatelessWidget {
   const ChangePasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kWhiteColor,
+      appBar: customAppBar(
+        context: context,
+        isLarge: true,
+        title: 'Change Password',
+      ),
+      body: const ChangePasswordViewBody(),
+    );
+  }
+}
+
+class ChangePasswordViewBody extends StatelessWidget {
+  const ChangePasswordViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
