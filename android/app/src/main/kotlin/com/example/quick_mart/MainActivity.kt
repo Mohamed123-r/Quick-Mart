d05c85e63effde82273853461b4cde749b251699
mohamed <mezoattalla123@gmail.com>
2024-09-07 23:16:32 +0300

    Replace FlutterActivity with FlutterFragmentActivity in MainActivity class

--- android/app/src/main/kotlin/com/example/quick_mart/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/quick_mart/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.quick_mart
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
