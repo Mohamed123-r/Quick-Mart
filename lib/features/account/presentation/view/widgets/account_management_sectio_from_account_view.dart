4a4fafbbfae94f08a93b3f14eb626455901301c4
mohamed <mezoattalla123@gmail.com>
2024-09-13 22:36:10 +0300

    Remove unused imports and comment out AccountItemList widget in AccountManagementSectionFromAccountView.

--- lib/features/account/presentation/view/widgets/account_management_sectio_from_account_view.dart
+++ lib/features/account/presentation/view/widgets/account_management_sectio_from_account_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quick_mart/core/utils/assets.dart';
 import 'package:quick_mart/core/utils/styles.dart';
-import 'package:quick_mart/features/account/presentation/view/widgets/account_item_list.dart';
 import 'package:quick_mart/features/account/presentation/view/widgets/switch_form_account_item.dart';
 
 class AccountManagementSectionFromAccountView extends StatelessWidget {
@@ -11,25 +9,25 @@ class AccountManagementSectionFromAccountView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Padding(
+        Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.0),
           child: Text(
             "Account Management",
             style: Styles.captionSemiBold,
           ),
         ),
-        const SizedBox(
+        SizedBox(
           height: 12,
         ),
-        AccountItemList(
-          title: 'Change Password',
-          iconLeading: Assets.changePasswordIcon,
-          onTap: () {},
-        ),
-        const SwitchFromAccountItem(),
+        // AccountItemList(
+        //   title: 'Change Password',
+        //   iconLeading: Assets.changePasswordIcon,
+        //   onTap: () {},
+        // ),
+        SwitchFromAccountItem(),
       ],
     );
   }
